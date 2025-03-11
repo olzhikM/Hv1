20ec305310886af443c9196e1bebb154568385e5
Arman <atyrau25051955@gmail.com>
2025-03-11 12:06:44 +0300

    Fixed folder structure, removed extra mt/mt/mt

--- mt/mt/mt/customers/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Customer(models.Model):
-    name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20, unique=True)
-    
-    def __str__(self):
-        return self.name
